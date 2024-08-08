0c9334aa7b8120d4828795f5482bd0ef516c877d
arjun7736 <arjunvengassery123@gmail.com>
2024-08-08 14:53:01 +0530

    update : jwt access changed

--- src/middleware/jwtVerification.ts
+++ src/middleware/jwtVerification.ts
@@ -1,30 +1,37 @@
-    import { Request, Response, NextFunction } from 'express';
-    import jwt from 'jsonwebtoken';
-    import { CustomError } from '../utils/error';
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { CustomError } from '../utils/error';
 
-    interface JwtPayload {
-    id: string;
-    }
-    declare module 'express-serve-static-core' {
-        interface Request {
-        user?: string; 
-        }
-    }
-    
-    const verifyToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies['token'];
+interface JwtPayload {
+  id: string;
+}
 
-    if (!token) {
-        throw new CustomError(401, 'Unauthorized');
-    }
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: string;
+  }
+}
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as JwtPayload;
-        req.user = decoded.id;
-        next();
-    } catch (err) {
-        throw new CustomError(403,"Invalied Token")
-    }
-    };
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
 
-    export default verifyToken;
+  if (!authHeader) {
+    throw new CustomError(401, 'Unauthorized');
+  }
+
+  const token = authHeader.startsWith('Bearer ') ? authHeader.substring(7) : null;
+
+  if (!token) {
+    throw new CustomError(401, 'Unauthorized');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as JwtPayload;
+    req.user = decoded.id;
+    next();
+  } catch (err) {
+    throw new CustomError(403, 'Invalid Token');
+  }
+};
+
+export default verifyToken;
