178821e323d2d833f586d6b1b4d0d4c8a82799dc
arjun7736 <arjunvengassery123@gmail.com>
2024-08-10 11:10:15 +0530

    Update : jwt expire removed

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,5 +2,5 @@ import jwt from "jsonwebtoken"
 import mongoose from "mongoose"
 
 export const generateToken=(id:mongoose.Types.ObjectId):string=>{
-    return jwt.sign({id},process.env.JWT_SECRET_KEY as string,{expiresIn:"1h"})
+    return jwt.sign({id},process.env.JWT_SECRET_KEY as string)
 }
